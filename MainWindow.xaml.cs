978bc68f72e73a3a3fb52be787d2b199ffb76311
A.H. van Hensbergen <HENA01@rocmondriaan.nl>
2020-10-29 22:00:54 +0100

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace rekenmachine
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {\
+
+       
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+       
+
+        private void Button_Click_To_Add(object sender, RoutedEventArgs e)
+        {
+            bool test1 = int.TryParse(Getal1.Text, result: out int getal1);
+            bool test2 = int.TryParse(Getal2.Text, result: out int getal2);
+            if (test1 && test2)
+            {
+                int result = getal1 + getal2;
+                Result.Text =  result.ToString();
+            }
+            else
+            {
+
+            }
+        }
+        
+    }
+}
---
+++ models/Rekenaar.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rekenmachine.models
+{
+    class Rekenaar
+    {
+        private int Getal1;
+        private int Getal2;
+        public int TelOp()
+        {
+            return Getal1 + Getal2;
+        }
+
+        public bool IsGetal1(string txt)
+        {
+            return int.TryParse(txt, out Getal1);
+        }
+        public bool IsGetal2(string txt)
+        {
+            return int.TryParse(txt, out Getal1);
+        }
+
+        public int TrekAf()
+        {
+            return Getal1 - Getal2;
+        }
+    }
+}
